27dcfea08b0c7d149849495e179439052910d772
Richard Chen <rcchen97@gmail.com>
2015-04-02 15:57:07 -0500

    ass
    money

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,4 +1,20 @@
+import javax.swing.Box;
+import javax.swing.JFrame;
+
 
 public class FrontEnd {
-	// foo
+	//fields
+	private JFrame myFrame;
+	private Box myBox;
+	
+	//constructor
+	public FrontEnd(){
+		myFrame = new JFrame("FrontEnd");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setLocation(400,400);
+		
+		myBox = new Box(6);
+		
+		myFrame.add(myBox);
+	}
 }
