a2ff97602dd7e4fcb397044ce0430c036665f8de
Yuangwang <yuangwang2@gmail.com>
2015-04-02 15:58:22 -0500

    seg

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,4 +1,18 @@
+import java.util.List;
+
 
 public class BackEnd {
-
+	public List<String>getMessages(String username)
+	{
+		
+		
+	}
+	public void addMessage(String username,String msg)
+	{
+		Encoder.write()
+	}
+	public List<String>getUser()
+	{
+		return 
+	}
 }
