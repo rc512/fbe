d4a47cd02135c0e90bea24638dbe57020e6d93ba
Yuangwang <yuangwang2@gmail.com>
2015-04-07 15:00:29 -0500

    Revert "gui"
    This reverts commit 89c5253b57fa6efbb9a040b29182692c4d3a9cf6.
    
    Conflicts:
    	src/BackEnd.java
    	src/Driver.java
    	src/FrontEnd.java

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,20 +1,12 @@
 import java.util.List;
 
 
-
 public class BackEnd {
-<<<<<<< HEAD
 	public List<String>getMessages(String username)
 
-=======
-
-	public List<String> getMessages(String username)
->>>>>>> origin/master
 	{
-		
 		return null;
 		
-<<<<<<< HEAD
 		
 	}
 	public void addMessage(String username,String msg)
@@ -27,18 +19,5 @@ public class BackEnd {
 	{
 		return null; 
  
-=======
-	}
-	public void addMessage(String username,String msg)
-	{
-		Encoder.write()
-		
-	}
-	public List<String>getUser()
-	public List<String> getUsers()
-	{
-		return 
-		return null;
->>>>>>> origin/master
 	}
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,6 @@
-import javax.swing.SwingUtilities;
-
 
 import javax.swing.SwingUtilities;
 public class Driver {
-<<<<<<< HEAD
-=======
-
-public static void main (String[] args)
-{
-
-	
-}
->>>>>>> origin/master
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -19,9 +8,5 @@ public static void main (String[] args)
 			}
 		});
 	}
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 }
 
