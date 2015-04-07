cd3f4c96ecb42c540a0fde3f4c88d91600745e19
Yuangwang <yuangwang2@gmail.com>
2015-04-07 16:00:42 -0500

    GUI

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,8 +1,6 @@
-import javax.swing.Box;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import java.awt.Dimension;
 
+import javax.swing.*;
 
 public class FrontEnd {
 	//fields
@@ -14,21 +12,33 @@ public class FrontEnd {
 	public FrontEnd(){
 		myFrame = new JFrame("FrontEnd");
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setPreferredSize(new Dimension(600, 100));
 		myFrame.setLocation(400,400);
 		
+		JPanel p=new JPanel();
+		p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
+		JTextArea searchDef = new JTextArea();
+		searchDef.setWrapStyleWord(true);
 		myBox = Box.createHorizontalBox();
 		myBox2 = Box.createHorizontalBox();
 		JTextField myuser = new JTextField();
-		JTextField mypass = new JTextField();
+		JButton input = new JButton("Add Message");
+		JButton get = new JButton("Get Messages");
 		
-		JLabel myusername = new JLabel("username");
-		JLabel mypassword = new JLabel("password");
+		JLabel myusername = new JLabel("Username");
+		JLabel mymessage = new JLabel("Message");
 		myBox.add(myusername);
 		myBox.add(myuser);
-		myBox2.add(mypassword);
-		myBox2.add(mypass);
-		myFrame.add(myBox);
-		myFrame.add(myBox2);
+		myBox2.add(mymessage);		
+		myBox2.add(searchDef);
+		myBox2.add(input);
+		myBox2.add(get);
+		
+		p.add(myBox);
+		p.add(myBox2);
+		
+		myFrame.add(p);
+		//myFrame.add(myBox2);
 		myFrame.pack();
 		myFrame.setVisible(true);
 	}
