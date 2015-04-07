294ff1d1c89fc658ce5596ac16a071a5bfea74f1
KeyMcD <keatonmcd@gmail.com>
2015-04-07 14:54:14 -0500

    ll

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,4 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
 
-public class Encoder {
+
+public class Encoder 
+{
+	public Encoder(String filename)
+	{
+		 
+	}
+	
+	public void write(String plaintext)
+	{
+		return;
+	}
+	
+	public List<String> read()
+	{
+		return null;
+		
+	}
 
 }
