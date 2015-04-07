2171745e8c731f7e244b39c668fc3aee10f8fb5b
Yuangwang <yuangwang2@gmail.com>
2015-04-07 15:03:34 -0500

    a

--- src/Driver.java
+++ src/Driver.java
@@ -1,27 +1,6 @@
-<<<<<<< HEAD
 
 import javax.swing.SwingUtilities;
 public class Driver {
-=======
-import javax.swing.SwingUtilities;
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 0567890... 420
-
-import javax.swing.SwingUtilities;
-public class Driver {
-<<<<<<< HEAD
-=======
-
-public static void main (String[] args)
-{
-	SwingUtilities.invokeLater(new Runnable() {
-		public void run() { new FrontEnd(); }
-	});
-}
->>>>>>> origin/master
->>>>>>> origin/master
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -30,4 +9,3 @@ public static void main (String[] args)
 		});
 	}
 }
-
