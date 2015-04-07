056789011d6d5eda2757d0cc565f8a5451fcff16
Richard Chen <rcchen97@gmail.com>
2015-04-07 14:49:50 -0500

    420
    idk

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -2,17 +2,17 @@ import java.util.List;
 
 
 public class BackEnd {
-	public List<String>getMessages(String username)
+	public List<String> getMessages(String username)
 	{
-		
+		return null;
 		
 	}
 	public void addMessage(String username,String msg)
 	{
-		Encoder.write()
+		
 	}
-	public List<String>getUser()
+	public List<String> getUsers()
 	{
-		return 
+		return null;
 	}
 }
